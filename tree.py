97e86f922bac5ca10cd56133d9e39a8131485dd8
Camila <camilacaleones@dcc.ufrj.br>
2022-06-03 19:10:36 -0300

    :memo: ED

--- tree.py
+++ tree.py
@@ -24,7 +24,36 @@ class BinaryTree:
         if node is None:
             node = self.root #percorra a partir da raiz
         if node.left:
+            print('(', end='')
             self.simetric_traversal(node.left)
-        print(node)
+        print(node, end='')
         if node.right:
             self.simetric_traversal(node.right)
+            print(')', end='')
+    
+    #percorre a árvore em pós-ordem: esq -> dir -> raiz
+    def postorder_traversal(self,node=None):
+        if node is None:
+            node = self.root
+        if node.lef:
+            self.postorder_traversal(node.left)
+            print('(', end='')
+        print(node, end='')
+        if node.right:
+            self.postorder_traversal(node.right)
+            print('(',end='')
+        print(node)
+    
+    #calcular a altura é ver a altura da esq e da direita é bom fazer isso com o pos ordem
+        def height(self,node=None):
+            if node is None:
+                node = self.root
+            hleft = hright = 0;
+            if node.lef:
+                hleft = self.height(node.left)
+            print(node, end='')
+            if node.right:
+                hright = self.height(node.right)
+            if hright > hleft:
+                return hright+1
+            return hleft+1
