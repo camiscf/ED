8c564053ec86bc29fd7e80dfa66a8ee988974c72
Camila <camilacaleones@dcc.ufrj.br>
2022-06-04 21:40:30 -0300

    :memo: ED

--- linkedList.cpp
+++ linkedList.cpp
@@ -7,6 +7,7 @@ class Node{
     Node *next;
 }
 
+//aqui insere no primeiro e muda a cabeça para o primeiro
 void push(Node** head, int new_info){
     Node* new_node = new Node();
 
@@ -17,6 +18,106 @@ void push(Node** head, int new_info){
     (*head) = new_node;
 }
 
-int main(void){
+//insere no canto qualquer e ajusta
 
+// Given a node prev_node, insert a
+// new node after the given
+// prev_node
+void insertAfter(Node* prev_node, int new_data)
+{
+ 
+    // 1. Check if the given prev_node is NULL
+    if (prev_node == NULL) {
+        cout << "The given previous node cannot be NULL";
+        return;
+    }
+ 
+    // 2. Allocate new node
+    Node* new_node = new Node();
+ 
+    // 3. Put in the data
+    new_node->data = new_data;
+ 
+    // 4. Make next of new node as
+    // next of prev_node
+    new_node->next = prev_node->next;
+ 
+    // 5. move the next of prev_node
+    // as new_node
+    prev_node->next = new_node;
+}
+
+/* Given a reference (pointer to pointer) to the head
+of a list and an int, appends a new node at the end */
+void append(Node** head_ref, int new_data)
+{
+    /* 1. allocate node */
+    Node* new_node = new Node();
+ 
+    Node *last = *head_ref; /* used in step 5*/
+ 
+    /* 2. put in the data */
+    new_node->data = new_data;
+ 
+    /* 3. This new node is going to be
+    the last node, so make next of
+    it as NULL*/
+    new_node->next = NULL;
+ 
+    /* 4. If the Linked List is empty,
+    then make the new node as head */
+    if (*head_ref == NULL)
+    {
+        *head_ref = new_node;
+        return;
+    }
+ 
+    /* 5. Else traverse till the last node */
+    while (last->next != NULL)
+    {
+        last = last->next;
+    }
+ 
+    /* 6. Change the next of last node */
+    last->next = new_node;
+    return;
+}
+void printList(Node *node)
+{
+    while (node != NULL)
+    {
+        cout<<" "<<node->data;
+        node = node->next;
+    }
+}
+ 
+/* Driver code*/
+int main()
+{
+    /* Start with the empty list */
+    Node* head = NULL;
+     
+    // Insert 6. So linked list becomes 6->NULL
+    append(&head, 6);
+     
+    // Insert 7 at the beginning.
+    // So linked list becomes 7->6->NULL
+    push(&head, 7);
+     
+    // Insert 1 at the beginning.
+    // So linked list becomes 1->7->6->NULL
+    push(&head, 1);
+     
+    // Insert 4 at the end. So
+    // linked list becomes 1->7->6->4->NULL
+    append(&head, 4);
+     
+    // Insert 8, after 7. So linked
+    // list becomes 1->7->8->6->4->NULL
+    insertAfter(head->next, 8);
+     
+    cout<<"Created Linked list is: ";
+    printList(head);
+     
+    return 0;
 }
